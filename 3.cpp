16644876c338cd44f81f38a496f2febbc262bcb2
MelynLyu <melynlyu@gmail.com>
2021-10-13 19:35:24 +0800

    3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,28 +1,45 @@
-#include <bits/stdc++.h>
+#include <bits./stdc++.h>
 
 using namespace std;
 
-int func1(long long n) {
-    if(n <= 1) {
-        return 1;
-    }
+string encode(string s) {
+    int n = s.size();
+    vector<vector<string>>dp (n, vector<string>(n, ""));
 
-    if(n % 2 == 0) {
-        return func1(n/2);
+    for(int len = 1; len <= n; ++len) {
+        for(int i = 0; i < n - len + 1; ++i) {
+            int j = i + len - 1;
+            string tmp = s.substr(i, len);
+            string rep = "";
+            dp[i][j] = tmp;
+            for(int k = i; k < j; ++k) {
+                string leftString = dp[i][k];
+                string rightString = dp[k+1][j];
+                if(leftString.size() + rightString.size() < dp[i][j].size()) {
+                    dp[i][j] = leftString + rightString;
+                }
+            }
+            
+            auto pos = (tmp + tmp).find(tmp, 1);
+            if(pos >= tmp.size()) {
+                rep = tmp;
+            }
+            else {
+                rep = to_string(tmp.size() / pos) + '[' + dp[i][i+pos-1] + ']';
+            }
+            if(rep.size() < dp[i][j].size()) {
+                dp[i][j] = rep;
+            }
+        }
+        
     }
-    else{
-        return 1 + func1((n-1)/2);
-    }
-}
-
-int func2(long long n) {
-    int k = (int)ceil(log2(n))
-    long long base = pow(2, k);
-    return 1 + func1(base - n);
+    return dp[0][n-1];
 }
 
 int main(void) {
-    long long n;
-    cin >> n;
-    cout << min(func1(n), func2(n)) << endl;
+    int ans = 0;
+    string s;
+    cin >> s;
+    cout << encode(s).size() << endl;
+    return 0;
 }
